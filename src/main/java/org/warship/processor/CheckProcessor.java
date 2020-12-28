9b93eaf957b3ed88d749506ba70ca3c28f858eb8
xidaokun <xidaokun@elastos.org>
2020-12-28 21:06:43 +0800

    tag v0.0.12

--- src/main/java/org/warship/processor/CheckProcessor.java
+++ src/main/java/org/warship/processor/CheckProcessor.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.AnnotationMirror;
@@ -20,6 +21,13 @@ import javax.lang.model.util.ElementFilter;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @AutoService(Processor.class)
+@SupportedAnnotationTypes({"org.warship.annotation.interfaces.Version",
+		"org.warship.annotation.interfaces.Author",
+		"org.warship.annotation.interfaces.Branch",
+		"org.warship.annotation.interfaces.CommitId",
+		"org.warship.annotation.interfaces.Date",
+		"org.warship.annotation.interfaces.Description",
+		"org.warship.annotation.interfaces.VersionCode"})
 public class CheckProcessor extends AbstractProcessor {
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
